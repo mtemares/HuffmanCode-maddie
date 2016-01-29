f7f53ba49e8a5bf78c0e3dfe46a7a292a8c0f8e3
mtemares <mtemares2016@pingry.org>
2016-01-29 12:29:00 -0500

    Create HuffmanRunner.java

---
+++ HuffmanRunner.java
@@ -0,0 +1,13 @@
+public class HuffmanRunner
+{
+	public static void main(String[] args)
+	{
+		HuffmanNode test = new HuffmanNode("b", 5);
+		test.setValue("c");
+		test.setFrequency(6);
+		System.out.println(test.value());
+		System.out.println(test.frequency());
+		HuffmanNode test2 = new HuffmanNode("d", 10);
+		System.out.println(test.compareTo(test2));
+	}
+}
