dc653e6f34e918d35de05d4d77b39817d515e6b9
mtemares <mtemares2016@pingry.org>
2016-01-29 12:26:49 -0500

    Update HuffmanNode.java

--- HuffmanNode.java
+++ HuffmanNode.java
@@ -8,58 +8,115 @@ public class HuffmanNode implements Comparable
 	protected String value;
 	protected HuffmanNode left;
 	protected HuffmanNode right;
-	
+	/*
+	@param
+	@return
+	*/
 	public HuffmanNode(String v, int f)
 	{
 		value = v;
 		frequency = f;
+		left = null;
+		right = null;
 	}
 	
+	/*
+	@param
+	@return
+	*/
+	public HuffmanNode(HuffmanNode l, HuffmanNode r)
+	{
+		left = l;
+		right = r;
+		value = l.value() + r.value();
+		frequency = l.frequency() + r.frequency();
+	}
+	
+	/*
+	@param
+	@return
+	*/
 	public HuffmanNode(String v)
 	{
 		value = v;
 	}
 	
+	/*
+	@param
+	@return
+	*/
 	public int frequency()
 	{
 		return frequency;
 	}
 	
+	/*
+	@param
+	@return
+	*/
 	public void setFrequency(int f)
 	{
 		frequency = f;
 	}
 		
+	/*
+	@param
+	@return
+	*/
 	public String value()
 	{
 		return value;
 	}
 	
+	/*
+	@param
+	@return
+	*/
 	public void setValue(String v)
 	{
 		value = v;
 	}
 	
+	/*
+	@param
+	@return
+	*/
 	public HuffmanNode left()
 	{
 		return left;
 	}
 	
+	/*
+	@param
+	@return
+	*/
 	public void setLeft(HuffmanNode l)
 	{
 		left = l;
 	}
 	
+	/*
+	@param
+	@return
+	*/
 	public HuffmanNode right()
 	{
 		return right;
 	}
 	
+	/*
+	@param
+	@return
+	*/
 	public void setRight(HuffmanNode r)
 	{
 		right = r;
 	}
 	
+	/*
+	@param
+	@return
+	*/
 	public int compareTo(Object o)
 	{
 		return frequency - ((HuffmanNode)o).frequency();
